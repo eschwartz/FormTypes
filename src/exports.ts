91e143fe10aa22be6e5a3af432737b1f4459489e
Edan Schwartz <edan@edanschwartz.com>
2015-03-06 17:12:11 -0600

    Export all FormTypes

--- src/exports.ts
+++ src/exports.ts
@@ -1,5 +1,15 @@
 ///ts:ref=node.d.ts
 /// <reference path="../typings/generated/node/node.d.ts"/> ///ts:ref:generated
 import FormType = require('./FormType/FormType');
+import FieldType = require('./FormType/FieldType');
+import TextType = require('./FormType/TextType');
+import ChoiceType = require('./FormType/ChoiceType');
+import OptionType = require('./FormType/OptionType');
 
-global.window.FormType = FormType;
+global.window.FormTypes = {
+  FormType: FormType,
+  FieldType: FieldType,
+  TextType: TextType,
+  ChoiceType: ChoiceType,
+  OptionType: OptionType
+};
