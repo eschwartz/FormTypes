321b4a8174e6babca4fbda6c9d0bbe82f10ba53e
Edan Schwartz <edan@edanschwartz.com>
2015-03-06 09:32:40 -0600

    {{partial_widget form}} helper

---
+++ src/View/TemplateHelper/PartialWidgetHelperFactory.ts
@@ -0,0 +1,21 @@
+///ts:ref=handlebars.d.ts
+/// <reference path="../../../typings/handlebars/handlebars.d.ts"/> ///ts:ref:generated
+import TemplateHelperInterface = require('./TemplateHelperInterface');
+import FormContextInterface = require('../Context/FormContextInterface');
+import Handlebars = require('handlebars');
+
+var PartialWidgetFactory = function(Handlebars:HandlebarsStatic):TemplateHelperInterface {
+  return function(form:FormContextInterface) {
+    var partial = form.type + '_widget';
+    var partialTemplate = Handlebars.partials[partial];
+
+    if (!partialTemplate) {
+      throw new Error('Unable to find partial for form of type ' + form.type);
+    }
+
+    return partialTemplate(form);
+  };
+};
+
+
+export = PartialWidgetFactory;
