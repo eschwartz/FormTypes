3d71b95f01337d13a3106a79f6e061855e425b67
Edan Schwartz <edan@edanschwartz.com>
2015-03-07 11:56:36 -0600

    minor test reformatting

--- test/spec/FormType/ChoiceTypeTest.ts
+++ test/spec/FormType/ChoiceTypeTest.ts
@@ -19,68 +19,71 @@ describe('ChoiceType', () => {
     $ = require('jquery');
   });
 
-  it('should create an empty select element', () => {
-    var $select:JQuery;
-    var choiceType = new ChoiceType();
+  describe('render', () => {
+    it('should create an empty select element', () => {
+      var $select:JQuery;
+      var choiceType = new ChoiceType();
 
-    choiceType.render();
-    $select = $(choiceType.el).find('select');
+      choiceType.render();
+      $select = $(choiceType.el).find('select');
 
-    assert.equal($select.length, 1,
-      'Expected a single select element to be rendered.');
+      assert.equal($select.length, 1,
+        'Expected a single select element to be rendered.');
 
-    assert.equal($select.children().length, 0,
-      'Expected select element to be empty.');
-  });
-
-  it('should create option elements for each choice', () => {
-    var $select:JQuery, $options:JQuery;
-    var choiceType = new ChoiceType({
-      choices: {
-        us: 'United States',
-        ca: 'Canada'
-      }
+      assert.equal($select.children().length, 0,
+        'Expected select element to be empty.');
     });
 
-    choiceType.render();
-    $select = $(choiceType.el).find('select');
-    $options = $select.children('option');
+    it('should create option elements for each choice', () => {
+      var $select:JQuery, $options:JQuery;
+      var choiceType = new ChoiceType({
+        choices: {
+          us: 'United States',
+          ca: 'Canada'
+        }
+      });
 
-    assert.equal($options.length, 2,
-      'Expected option element to be created for each choice');
+      choiceType.render();
+      $select = $(choiceType.el).find('select');
+      $options = $select.children('option');
 
-    // NOTE: these tests are fragile,
-    //   as not all browsers may maintain object
-    //   ordering they way you'd expect.
-    assert.equal($options.eq(0).val(), 'us');
-    assert.equal($options.eq(1).val(), 'ca');
+      assert.equal($options.length, 2,
+        'Expected option element to be created for each choice');
 
-    assert.equal($options.eq(0).text().trim(), 'United States');
-    assert.equal($options.eq(1).text().trim(), 'Canada');
-  });
+      // NOTE: these tests are fragile,
+      //   as not all browsers may maintain object
+      //   ordering they way you'd expect.
+      assert.equal($options.eq(0).val(), 'us');
+      assert.equal($options.eq(1).val(), 'ca');
 
-  it('should select the specified option', () => {
-    var $select:JQuery, $options:JQuery;
-    var choiceType = new ChoiceType({
-      choices: {
-        us: 'United States',
-        fr: 'France',
-        ca: 'Canada'
-      },
-      data: 'fr'
+      assert.equal($options.eq(0).text().trim(), 'United States');
+      assert.equal($options.eq(1).text().trim(), 'Canada');
     });
 
-    choiceType.render();
-    $select = $(choiceType.el).find('select');
-    $options = $select.children('option');
+    it('should select the specified option', () => {
+      var $select:JQuery, $options:JQuery;
+      var choiceType = new ChoiceType({
+        choices: {
+          us: 'United States',
+          fr: 'France',
+          ca: 'Canada'
+        },
+        data: 'fr'
+      });
 
-    // NOTE: these tests are fragile,
-    //   as not all browsers may maintain object
-    //   ordering they way you'd expect.
-    assert.equal($options.eq(1).val(), 'fr');
-    assert(!$options.eq(0).prop('selected'), 'Expected `us` not be selected');
-    assert($options.eq(1).prop('selected'), 'Expected `fr` to be selected');
-    assert(!$options.eq(2).prop('selected'), 'Expected `ca` not to be selected');
+      choiceType.render();
+      $select = $(choiceType.el).find('select');
+      $options = $select.children('option');
+
+      // NOTE: these tests are fragile,
+      //   as not all browsers may maintain object
+      //   ordering they way you'd expect.
+      assert.equal($options.eq(1).val(), 'fr');
+      assert(!$options.eq(0).prop('selected'), 'Expected `us` not be selected');
+      assert($options.eq(1).prop('selected'), 'Expected `fr` to be selected');
+      assert(!$options.eq(2).prop('selected'), 'Expected `ca` not to be selected');
+    });
   });
+
 
 });
