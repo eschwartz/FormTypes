4805f7ffee76f06674efcef653f85a122e626c74
Edan Schwartz <eschwartz@weathernation.net>
2015-06-30 14:39:33 -0500

    Use JQueryHtmlEvents for all tests

---
+++ test/spec/initTest.ts
@@ -0,0 +1,5 @@
+
+import ServiceContainer = require('../../src/Service/ServiceContainer');
+import JQueryHtmlEvents = require('../Util/JQueryHtmlEvents');
+
+ServiceContainer.HtmlEvents = JQueryHtmlEvents;
